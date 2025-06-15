a4f7198b71c4901ddf4d39eadb257cdf7f0f6ad0
Renan Mendes <142457377+ReMendess@users.noreply.github.com>
2025-06-15 15:58:07 -0300

    Update appy.py

--- Modelagem_Preditiva/appy.py
+++ Modelagem_Preditiva/appy.py
@@ -1,1 +1,15 @@
 
+import streamlit as st
+from pagina_1_exploratoria import mostrar_pagina_exploratoria
+from pagina_2_modelo import mostrar_pagina_modelo
+
+st.set_page_config(page_title="Monitoramento de Sensores", layout="wide")
+
+paginas = {
+    "Análise Exploratória": mostrar_pagina_exploratoria,
+    "Análise Preditiva": mostrar_pagina_modelo
+}
+
+pagina = st.sidebar.radio("Escolha a página:", list(paginas.keys()))
+paginas[pagina]()
+
